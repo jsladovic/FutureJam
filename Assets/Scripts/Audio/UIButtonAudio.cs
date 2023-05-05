8f9ed4942405c5aad2fa523daf4c2aa66cfcf3b1
bionucular <ilicluka074@gmail.com>
2023-05-05 11:23:00 +0200

    Added UI Button Audio class

---
+++ Assets/Scripts/Audio/UIButtonAudio.cs
@@ -0,0 +1,31 @@
+﻿using FMODUnity;
+using UnityEngine;
+using UnityEngine.EventSystems;
+
+namespace Assets.Scripts.Audio
+{
+	public class UIButtonAudio : MonoBehaviour, IPointerDownHandler, IPointerEnterHandler
+	{
+		[SerializeField] private EventReference HoverSound;
+		[SerializeField] private EventReference PressSound;
+		private bool DisableAudio = false;
+
+		private void Start()
+		{
+            if (RuntimeManager.HasBankLoaded("Master") == false)
+                RuntimeUtils.DebugLogWarning("Master audio bank not loaded");
+        }
+
+		public void OnPointerEnter(PointerEventData eventData)
+        {
+			if (DisableAudio == false && HoverSound.IsNull == false)
+				RuntimeManager.PlayOneShot(HoverSound);
+		}
+        public void OnPointerDown(PointerEventData eventData)
+        {
+            if (DisableAudio == false && PressSound.IsNull == false)
+                RuntimeManager.PlayOneShot(PressSound);
+        }
+
+    }
+}
