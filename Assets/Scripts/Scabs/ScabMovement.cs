eff706fb9b66012cdc7f4643b08f9f8e7ab97366
Janko Sladovic <janko.sladovic@gmail.com>
2023-05-24 19:11:48 +0200

    Increased scab speed when leaving

--- Assets/Scripts/Scabs/ScabMovement.cs
+++ Assets/Scripts/Scabs/ScabMovement.cs
@@ -4,69 +4,76 @@ using UnityEngine;
 namespace Assets.Scripts.Scabs
 {
 	public class ScabMovement : MonoBehaviour
-    {
-        private Scab Parent;
-        private MovementCurve Curve;
+	{
+		private Scab Parent;
+		private MovementCurve Curve;
 
-        private float Speed;
+		private bool UseLeavingSpeed;
+		private float Speed;
+		private float LeavingSpeed;
 
-        public MovementState State { get; set; }
-        public object OnBuildingEndtered { get; internal set; }
+		public MovementState State { get; set; }
+		public object OnBuildingEndtered { get; internal set; }
 
-        private Vector3? TargetPosition;
-        private const float DistanceToPointThreshold = 0.1f;
+		private Vector3? TargetPosition;
+		private const float DistanceToPointThreshold = 0.1f;
+		private const float LeavingSpeedMultiplier = 1.25f;
 
-        public void Initialize(Scab parent, MovementCurve curve, float speed)
-        {
-            Parent = parent;
-            State = MovementState.Entering;
-            Curve = curve;
-            Speed = speed;
-            transform.position = curve.Points[0].transform.position;
-        }
+		public void Initialize(Scab parent, MovementCurve curve, float speed)
+		{
+			Parent = parent;
+			State = MovementState.Entering;
+			Curve = curve;
+			Speed = speed;
+			LeavingSpeed = speed * LeavingSpeedMultiplier;
+			transform.position = curve.Points[0].transform.position;
+			UseLeavingSpeed = false;
+		}
 
-        void Update()
-        {
-            if (Curve == null)
-                return;
-            switch (State)
-            {
-                case MovementState.Entering:
-                    HandleEntering();
-                    return;
-                case MovementState.Entered:
-                case MovementState.Leaving:
-                    HandleMovementToTargetPosition();
-                    return;
-            }
-        }
+		void Update()
+		{
+			if (Curve == null)
+				return;
+			switch (State)
+			{
+				case MovementState.Entering:
+					HandleEntering();
+					return;
+				case MovementState.Entered:
+				case MovementState.Leaving:
+					HandleMovementToTargetPosition();
+					return;
+			}
+		}
 
-        private void HandleEntering()
-        {
-            MoveTowardsPoint(Curve.Points[1].transform.position);
-        }
+		private void HandleEntering()
+		{
+			MoveTowardsPoint(Curve.Points[1].transform.position);
+		}
 
-        private void HandleMovementToTargetPosition()
-        {
-            if (TargetPosition.HasValue == false)
-                TargetPosition = ExitPointsController.Instance.GetNearestExitPoint(transform.position);
-            MoveTowardsPoint(TargetPosition.Value);
-            if (Vector3.Distance(transform.position, TargetPosition.Value) <= DistanceToPointThreshold)
-            {
-                Destroy(Parent.gameObject);
-                GameController.Instance.OnScabDestroyed();
-            }
-        }
+		private void HandleMovementToTargetPosition()
+		{
+			UseLeavingSpeed = true;
+			if (TargetPosition.HasValue == false)
+				TargetPosition = ExitPointsController.Instance.GetNearestExitPoint(transform.position);
+			MoveTowardsPoint(TargetPosition.Value);
+			if (Vector3.Distance(transform.position, TargetPosition.Value) <= DistanceToPointThreshold)
+			{
+				Destroy(Parent.gameObject);
+				GameController.Instance.OnScabDestroyed();
+			}
+		}
 
-        public void OnBuildingEntered(Vector3 doorPosition)
-        {
-            State = MovementState.Entered;
-            TargetPosition = doorPosition;
-        }
+		public void OnBuildingEntered(Vector3 doorPosition)
+		{
+			State = MovementState.Entered;
+			TargetPosition = doorPosition;
+		}
 
-        private void MoveTowardsPoint(Vector3 point)
-        {
-            transform.position = Vector3.MoveTowards(transform.position, point, Time.deltaTime * Speed);
-        }
-    }
+		private void MoveTowardsPoint(Vector3 point)
+		{
+			float speed = UseLeavingSpeed ? LeavingSpeed : Speed;
+			transform.position = Vector3.MoveTowards(transform.position, point, Time.deltaTime * speed);
+		}
+	}
 }
