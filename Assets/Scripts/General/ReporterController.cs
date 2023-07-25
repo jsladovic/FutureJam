b4b09e5543f9aac0821a04dd58e1f2095d159e4f
Janko Sladovic <janko.sladovic@gmail.com>
2023-07-25 19:26:01 +0200

    Reporters show up every few levels

---
+++ Assets/Scripts/General/ReporterController.cs
@@ -0,0 +1,23 @@
+﻿using UnityEngine;
+
+namespace Assets.Scripts.General
+{
+	[RequireComponent(typeof(SpriteRenderer))]
+	public class ReporterController : MonoBehaviour
+	{
+		[SerializeField] private int ShowsUpLevelIndex;
+
+		private SpriteRenderer SpriteRenderer;
+
+		private void Awake()
+		{
+			SpriteRenderer = GetComponent<SpriteRenderer>();
+			SpriteRenderer.enabled = false;
+		}
+
+		public void OnLevelStarted(LevelDefinition levelDefinition)
+		{
+			SpriteRenderer.enabled = levelDefinition.Index >= ShowsUpLevelIndex;
+		}
+	}
+}
