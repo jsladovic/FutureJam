c63dc1d917e80e359dd9bb83790c60eea8f6b2cd
Janko Sladovic <janko.sladovic@gmail.com>
2023-08-20 18:42:52 +0200

    Fixed bug after exiting game to main menu

--- Assets/Scripts/MainMenu/MainMenuController.cs
+++ Assets/Scripts/MainMenu/MainMenuController.cs
@@ -24,6 +24,7 @@ namespace Assets.Scripts.MainMenu
 
 		private void Awake()
 		{
+			Time.timeScale = 1.0f;
 			CanvasGroup = GetComponent<CanvasGroup>();
 			CanvasGroup.Disable();
 			MainMenu.Enable();
